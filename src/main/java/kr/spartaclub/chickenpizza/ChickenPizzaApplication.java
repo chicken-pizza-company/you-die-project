fc12cd6db3232bfea84143dc680c10dc0dc2542c
soongu <naong526@naver.com>
2026-02-19 16:26:27 +0900

    initial commit

---
+++ src/main/java/kr/spartaclub/chickenpizza/ChickenPizzaApplication.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.chickenpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChickenPizzaApplication.class, args);
+    }
+
+}
